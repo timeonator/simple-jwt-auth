15096a6857e59c46160392cceeea2c4ff8676659
timeonator <timeonator@gmail.com>
2021-01-30 14:26:18 -0800

    initial connection to mysql. Still formative.

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,11 +1,45 @@
+"use strict"
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+const getUser = require('./authService');
+var mysql = require('mysql')
+// var con = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'lull5gogM#',
+//   database: 'pikto'
+// });
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//   });
+const userService = require('./authService');
 
 const accessTokenSecret = 'youraccesstokensecret';
 const refreshTokenSecret = 'yourrefreshtokensecrethere';
 var refreshTokens = [];
+
+const users = [
+    {
+        email: 'john',
+        password: 'password123admin',
+        role: 'admin'
+    }, {
+        email: 'anna',
+        password: 'password123member',
+        role: 'member'
+    }
+];
+// function getUser(email, password=''){
+//     console.log(email,password);
+ 
+//     // Filter user from the users array by email and password
+//     const user = users.find(u => { return u.email === email && u.password === password });
+
+//     return user;  
+// }
 
 app.use(bodyParser.json());
 
@@ -13,37 +47,29 @@ app.use(bodyParser.json());
 app.listen(3000, () => {
     console.log('Authentication service started on port 3000');
 });
+
 app.post('/login', (req, res) => {
-    const users = [
-        {
-            username: 'john',
-            password: 'password123admin',
-            role: 'admin'
-        }, {
-            username: 'anna',
-            password: 'password123member',
-            role: 'member'
-        }
-    ];
-    // Read username and password from request body
-    const { username, password } = req.body;
-    console.log(username,password);
- 
-    // Filter user from the users array by username and password
-    const user = users.find(u => { return u.username === username && u.password === password });
-
-    if (user) {
-        // Generate an access token
-        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret, {expiresIn: '12ms'});
-        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
-
-        refreshTokens.push(refreshToken);
-        res.send({
-            accessToken, refreshToken
-        });
-    } else {
-        res.send('Username or password incorrect');
+    let accessToken="eat shit dog", refreshToken;
+    function table(row) {    
+        console.log(accessToken);
+        accessToken = jwt.sign({ email: row.email }, accessTokenSecret, {expiresIn: '12ms'});
+        refreshToken = jwt.sign({ email: row.email }, refreshTokenSecret);
+        return accessToken;
     }
+
+
+    // Read email and password from request body
+    const { email, password } = req.body;
+
+    // Filter user from the users array by email[] and password
+    getUser(email,password, row => {
+        console.log(accessToken);
+        accessToken = jwt.sign({ email: row.email }, accessTokenSecret, {expiresIn: '12ms'});
+        refreshToken = jwt.sign({ email: row.email }, refreshTokenSecret);
+        refreshTokens.push(refreshToken);
+        console.log(accessToken);
+        res.send({accessToken});
+    });
 });
 
 app.post('/logout', (req, res) => {
@@ -70,7 +96,7 @@ app.post('/token', (req, res) => {
             return res.sendStatus(403);
         }
 
-        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+        const accessToken = jwt.sign({ email: user.email, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
 
         res.json({
             accessToken
---
+++ src/service/authService.js
@@ -0,0 +1,121 @@
+require('dotenv').config();
+var mysql = require('mysql');
+const bcrypt = require('bcrypt');
+var mysql = require('mysql');
+
+var sql = mysql.createConnection({
+    host:process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_DB
+});
+
+function getUser(email, password, callback) {
+    let query=`SELECT * FROM users WHERE email=\'${email}\' AND password=\'${password}\';`;
+  
+    sql.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+      });
+
+    console.log(query);
+    sql.query(query, function (err, result, fields) {
+        if (err) {
+            throw err;
+        }
+        callback(result[0]);
+    });
+};
+
+module.exports = getUser;
+
+// app.get('/password/:pw', cors(corsOptions), (req, res) => {
+//     const saltRounds = 10;
+//     const myPlaintextPassword = req.params.pw;
+//     bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+//         res.send(hash);
+//     });
+// });
+// app.put('/password/:id/:pw', cors(corsOptions), (req,res)=>{
+//     const saltRounds = 10;
+//     const myPlaintextPassword = req.params.pw;
+//     bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+//         const query = `update users set users.password=? where users.id=?`;
+//         console.log(query,[hash,req.params.id]);
+//         connection.query(query, [hash, req.params.id], (error, results, fields) => {
+//             if (error){
+//                 res.end();
+//                 return console.error(error.message);
+//             }  
+//             console.log('Updated ', results);
+//             res.end();
+//         });
+//     });
+// });
+
+
+// app.get('/users',  (req, res) => {
+//     const query = "SELECT * FROM users";
+//     connection.query(query, function (err, result, fields) {
+//         if (err) throw err
+//         console.log("Query Result: ", JSON.stringify(result));
+//         res.send(JSON.stringify(result));
+//     });
+// });
+
+// app.get('/user/:email', cors(corsOptions), function (req, res, next) {
+//     let query=`SELECT * FROM users where email=?`;
+
+//     console.log("Query params: ", JSON.stringify(req.params));
+//     connection.query(query, [req.params.email,req.params.password],  function (err, result, fields) {
+//         if (err) throw err
+//         console.log("Query Result: ", JSON.stringify(result));
+//         res.send(JSON.stringify(result));
+//     });
+//   });
+
+//   app.post('/user',  function(req, res) {
+//       let query=`insert into users set users.email = ?, users.password=?`;
+    
+//       console.log("Request Body: ", req.body);
+//       connection.query(query,[req.body.email,req.body.password], function(err, result, fields) {
+//           if (err) throw err;
+//           res.send(JSON.stringify(result));
+//       });
+
+//   });
+  
+// // app.get('/user/:id:email', (req, res, next) => {
+// //     console.log(JSON.stringify(req.params));
+// //     res.send(JSON.stringify(req.param));
+// // });
+
+// app.post('/user:index', (req, res) => {
+//     console.log(`post to /user port ${PORT}`);
+// });
+
+// app.post('/users/:first_name/:last_name/:email/:username', (req, res) => {
+//     let sql = `INSERT INTO users(first_name, last_name, email, username) VALUES( ?, ?, ?, ?)`;
+//     let p = req.params;
+//     // console.log(p);
+//     let params = [req.params.first_name, p.last_name, p.email, p.username];
+//     connection.query(sql, [p.first_name,p.last_name,p.email,p.username], (error, results, fields) => {
+//       if (error){
+//         return console.error(error.message);
+//         res.end();
+//       }  
+//       console.log('Added Row(s) from Users:', results.affectedRows);
+//       res.end();
+//     });
+// });
+
+// app.delete('/user/:id', (req, res) => {
+//     let sql = `DELETE FROM users WHERE users.id = ?`;
+//     connection.query(sql, Number(req.params.id), (error, results, fields) => {
+//       if (error)
+//         return console.error(error.message);
+        
+//       console.log('Deleted Row(s) from Users:', results.affectedRows);
+//       res.end();
+//     });
+// });
