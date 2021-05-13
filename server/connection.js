52dbb6371d4d2ddc985a747e45b01eecfd23778f
timeonator <timeonator@gmail.com>
2021-05-12 20:27:08 -0700

    remove server

--- server/connection.js
+++
@@ -1,8 +0,0 @@
-var mysql = require('mysql')
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'lull5gogM#',
-  database: 'pikto'
-});
-export default connection;
